b6d0c5c4bb5bc34d8e66e6c2c580255df21b6677
AnnaCherdan <100855612+AnnaCherdan@users.noreply.github.com>
2022-05-01 17:31:17 +0700

    16.9.1, 16.9.2

--- my_rectangle.py
+++ my_rectangle.py
@@ -14,3 +14,23 @@ class MyRectangle:
 
 rec = MyRectangle(5, 10, 50, 100)
 print(rec)
+print('-' * 20)
+
+
+class MyRectangle:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return f'Rectangle: {self.x}, {self.y}, {self.width}, {self.height}.'
+
+    def get_area(self):
+        return self.width * self.height
+
+
+rec = MyRectangle(5, 10, 50, 100)
+print(rec.get_area())
+print('-' * 20)
