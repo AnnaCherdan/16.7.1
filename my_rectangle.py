0de45da56f7ec5089abe732cf94d82bb5dbf1aa8
AnnaCherdan <100855612+AnnaCherdan@users.noreply.github.com>
2022-05-01 17:00:12 +0700

    16.9.1

---
+++ my_rectangle.py
@@ -0,0 +1,16 @@
+# начальные координаты x, y, width и height. x = 5, y = 10, width = 50, height = 100
+
+
+class MyRectangle:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return f'Rectangle: {self.x}, {self.y}, {self.width}, {self.height}.'
+
+
+rec = MyRectangle(5, 10, 50, 100)
+print(rec)
