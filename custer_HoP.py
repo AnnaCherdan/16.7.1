e7ca52d6eb4ac767dbcf807b554b7b94629e1a31
AnnaCherdan <100855612+AnnaCherdan@users.noreply.github.com>
2022-05-01 17:48:00 +0700

    16.9.3

--- custer_HoP.py
+++ custer_HoP.py
@@ -10,7 +10,7 @@ class Customer:
         self.balance = balance
 
     def __str__(self):
-        return f'{self.name} {self.surname}. {self.city}. Баланс: {self.balance} .'
+        return f'{self.name} {self.surname}. {self.city}. Баланс: {self.balance} руб.'
 
 
 cus_1 = Customer('Иван', "Петров", "Москва", 50)
