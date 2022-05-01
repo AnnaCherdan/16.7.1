3ffc04bf3f57c36444eb1248ec2e1d623fd57e4e
AnnaCherdan <100855612+AnnaCherdan@users.noreply.github.com>
2022-05-01 12:36:27 +0700

    differend cat's

---
+++ Cats.py
@@ -0,0 +1,14 @@
+from cat_at_HoP import Cat
+
+cat1 = Cat('Барон', "Мальчик", "2 года")
+
+print("Кота зовут:", cat1.name)
+print("Это -", cat1.gender)
+print("Его возраст:", cat1.age)
+print('-' * 20)
+
+cat2 = Cat('Сэм', "Мальчик", "2 года")
+
+print("Кота зовут:", cat2.name)
+print("Это -", cat2.gender)
+print("Его возраст:", cat2.age)
