d762e6094b0e7169fb8fc39086d16b15e952be64
AnnaCherdan <100855612+AnnaCherdan@users.noreply.github.com>
2022-05-01 17:44:30 +0700

    16.9.3

---
+++ custer_HoP.py
@@ -0,0 +1,17 @@
+# О клиенте известна следующая информация: имя, фамилия, город, баланс.
+# «Иван Петров. Москва. Баланс: 50 руб.»
+
+
+class Customer:
+    def __init__(self, name, surname, city, balance):
+        self.name = name
+        self.surname = surname
+        self.city = city
+        self.balance = balance
+
+    def __str__(self):
+        return f'{self.name} {self.surname}. {self.city}. Баланс: {self.balance} .'
+
+
+cus_1 = Customer('Иван', "Петров", "Москва", 50)
+print(cus_1)
